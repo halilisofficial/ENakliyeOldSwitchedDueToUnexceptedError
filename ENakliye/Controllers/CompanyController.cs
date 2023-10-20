0bd7c93696e854d657572d6565980abb9b53feb4
halil_is <halilisofficial@gmail.com>
2023-10-20 15:40:54 +0300

    Layout And login template added
    layout completed
    web site template complete
    login page template complete
    forget your password page template complete

---
+++ ENakliye/Controllers/CompanyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ENakliye.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OverView()//genel bakış
+        {
+            return View();
+        }
+        public IActionResult CustomerRequitCostemers()// müşteri müşteri kazandırır
+        {
+            return View();
+        }
+        public IActionResult BuisnessPartners()//İş Ortaklarımız
+        {
+            return View();
+        }
+        public IActionResult AboutUs()//Hakkımızda
+        {
+            return View();
+        }
+        public IActionResult SuccsessStories()//başarı hikayeleri
+        {
+            return View();
+        }
+        public IActionResult CustomerReferences()//Müşteri Referansları
+        {
+            return View();
+        }
+    }
+}
---
+++ ENakliye/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ENakliye.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
