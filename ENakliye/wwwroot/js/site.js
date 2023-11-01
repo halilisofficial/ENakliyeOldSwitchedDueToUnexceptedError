28223ce7a427873423ca6bcb7a92fb3dd41b397d
halil_is <halilisofficial@gmail.com>
2023-11-01 10:32:14 +0300

    servis security template and its partial views added

--- ENakliye/wwwroot/js/site.js
+++ ENakliye/wwwroot/js/site.js
@@ -1,5 +1,6 @@
-﻿var button = document.getElementById("scroll_to_top_button");
-var scrollThreshold = 300; // Eşik değeri (100 piksel)
+﻿/*yukarı scroll butonunda kullanılmaktadır _Layoutcshtml*/
+var button = document.getElementById("scroll_to_top_button");
+var scrollThreshold = 300; // Eşik değeri (300 piksel)
 
 // Sayfayı aşağı kaydırdıkça butonu görünür hale getir
 window.onscroll = function () {
